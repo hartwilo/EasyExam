9fdce666b50357f80ff85fba0fe92e7c68b17d36
82karu1bwi <82karu1bwi@hft-stuttgart.de>
2020-11-24 11:17:56 +0100

    fix connection in DBQuerys

--- src/main/java/DB/DBQuerys.java
+++ src/main/java/DB/DBQuerys.java
@@ -7,7 +7,6 @@ import java.sql.Statement;
 
 public class DBQuerys {
 
-	private Connection conn;
 	
 	/**
 	 * 
@@ -23,12 +22,10 @@ public class DBQuerys {
 	 */
 	public int frageSpeichern(String fragestellung, String musterloesung, int niveau, String punkte, String gestellt, String themengebiet, String fragekatalog, String modul) throws SQLException
 	{
-		
-		conn.setAutoCommit(false);
-		Statement stmt = conn.createStatement();
+		DBConn.connection.setAutoCommit(false);
+		Statement stmt = DBConn.connection.createStatement();
 		String query = "INSERT INTO Frage(Fragestellung, Musterloesung, Niveau, Punkte, gestellt, themengebiet, Fragekatalog, Modul) "
 				+ "Values('" + fragestellung + "','" + musterloesung + "', '" + niveau +"', '" + punkte + "', '" + gestellt + "', '" + themengebiet + "', '" + fragekatalog + "', '" + modul +"')";
-		System.out.println(query);
 		return stmt.executeUpdate(query);
 	}
 	
