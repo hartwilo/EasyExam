8920920cfc10aeea8db2f874b3e69e994849a917
82karu1bwi <82karu1bwi@hft-stuttgart.de>
2020-12-17 09:43:45 +0100

    add dependencies for mockito

---
+++ src/test/java/de/hftstuttgart/EasyExam/Controllers/LoginControllerTest.java
@@ -0,0 +1,34 @@
+package de.hftstuttgart.EasyExam.Controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LoginControllerTest {
+
+	@Test
+	void testHandleButtonAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLoginController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckBoxClick1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPasswordVergessenClick() {
+		fail("Not yet implemented");
+	}
+
+}
