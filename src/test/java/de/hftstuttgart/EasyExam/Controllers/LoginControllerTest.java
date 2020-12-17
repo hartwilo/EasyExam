d173f1e71f7c64d6311e004caa54a5099afa31a2
82karu1bwi <82karu1bwi@hft-stuttgart.de>
2020-12-17 11:27:16 +0100

    try to test LoginController

--- src/test/java/de/hftstuttgart/EasyExam/Controllers/LoginControllerTest.java
+++ src/test/java/de/hftstuttgart/EasyExam/Controllers/LoginControllerTest.java
@@ -2,10 +2,53 @@ package de.hftstuttgart.EasyExam.Controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.loadui.testfx.GuiTest;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.testfx.framework.junit5.ApplicationTest;
 
-class LoginControllerTest {
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 
+import DB.DBConn;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * 
+ * @author Ruth Kallenberger
+ *
+ */
+
+class LoginControllerTest extends GuiTest{
+	
+	LoginController lc;
+	
+	/*@Mock
+    LoginController login; 
+
+    @Rule public MockitoRule mockitoRule = MockitoJUnit.rule(); */
+    
+	@Test
+	void checkComponentsTest() {
+		JFXTextField UsernameTextField = find("#UsernameTextField");
+		assertTrue(UsernameTextField.getText().equals("Username oder E-mail"));
+//		JFXButton Login = find("#Login");
+//		assertTrue(Login.getText().equals("Absenden"));
+//		Label Willkommen = find("#Willkommen");
+//		assertTrue(Willkommen.getText().equals("Willkommen"));
+		JFXPasswordField PasswordField = find("#PasswordField");
+		assertTrue(PasswordField.getText().equals("Password"));
+//		JFXButton PasswordVergessen = find("#PasswordVergessen");
+//		assertTrue(PasswordVergessen.getText().equals("Password vergessen?"));
+	}
+	
 	@Test
 	void testHandleButtonAction() {
 		fail("Not yet implemented");
@@ -29,6 +72,17 @@ class LoginControllerTest {
 	@Test
 	void testPasswordVergessenClick() {
 		fail("Not yet implemented");
+	}
+
+	@Override
+	protected Parent getRootNode() {
+		Parent parent = null;
+		  try {
+		    parent = FXMLLoader.load(getClass().getResource("/views/main.fxml"));
+		  } catch (IOException ex) {
+		    ex.printStackTrace();
+		  }
+		  return parent;
 	}
 
 }
