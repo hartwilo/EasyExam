d4f3d7695c981d5d04d1001b094d816c785671bb
GjergjiSh <72801425+GjergjiSh@users.noreply.github.com>
2020-12-21 09:20:05 +0100

    sql skript update

--- resources/EasyExam_schema.sql
+++ resources/EasyExam_schema.sql
@@ -34,7 +34,9 @@ Fragekatalog VARCHAR(255),
 Modul VARCHAR(255),
 grundLageNiveau VARCHAR(255),
 gut VARCHAR(255),
-sehrGut VARCHAR(255));
+sehrGut VARCHAR(255),
+Notizen text(500) null,
+Punkte_erreicht FLOAT null);
 
 CREATE TABLE Pruefung
 (idPruefung INTEGER PRIMARY KEY,
