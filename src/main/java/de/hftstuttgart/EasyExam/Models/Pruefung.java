756082e24f802b24cd605a934e2055cb6556bf98
82karu1bwi <82karu1bwi@hft-stuttgart.de>
2021-01-11 17:38:21 +0100

    add javadoc comments

--- src/main/java/de/hftstuttgart/EasyExam/Models/Pruefung.java
+++ src/main/java/de/hftstuttgart/EasyExam/Models/Pruefung.java
@@ -9,7 +9,16 @@ public class Pruefung<Katalog> {
 	private Student student;
 	private Pruefer pruefer;
 	
-	
+	/**
+	 * Constructor with ID, bezeichnung, note, fragekatalog, student and pruefer
+	 * 
+	 * @param idPruefung
+	 * @param bezeichnung
+	 * @param note
+	 * @param fragekatalog
+	 * @param student
+	 * @param pruefer
+	 */
 	public Pruefung(int idPruefung, String bezeichnung, float note, Katalog fragekatalog, Student student,
 			Pruefer pruefer) {
 		super();
@@ -21,62 +30,110 @@ public class Pruefung<Katalog> {
 		this.pruefer = pruefer;
 	}
 
-
+	/**
+	 * The method is used to get the ID 
+	 * 
+	 * @return
+	 */
 	public int getIdPruefung() {
 		return idPruefung;
 	}
 
-
+	/**
+	 * The method is used to set the ID
+	 * 
+	 * @param idPruefung
+	 */
 	public void setIdPruefung(int idPruefung) {
 		this.idPruefung = idPruefung;
 	}
 
-
+	/**
+	 * The method is used to get the name 
+	 * 
+	 * @return
+	 */
 	public String getBezeichnung() {
 		return bezeichnung;
 	}
 
-
+	/**
+	 * The method is used to set the name
+	 * 
+	 * @param bezeichnung
+	 */
 	public void setBezeichnung(String bezeichnung) {
 		this.bezeichnung = bezeichnung;
 	}
 
-
+	/**
+	 * The method is used to get the grade 
+	 * 
+	 * @return
+	 */
 	public float getNote() {
 		return note;
 	}
 
-
+	/**
+	 * The method is used to set the grade
+	 * 
+	 * @param note
+	 */
 	public void setNote(float note) {
 		this.note = note;
 	}
 
-
+	/**
+	 * The method is used to get the catalog
+	 * 
+	 * @return
+	 */
 	public Katalog getFragekatalog() {
 		return fragekatalog;
 	}
 
-
+	/**
+	 * The method is used to set the catalog
+	 * 
+	 * @param fragekatalog
+	 */
 	public void setFragekatalog(Katalog fragekatalog) {
 		this.fragekatalog = fragekatalog;
 	}
-
-
+	
+	/**
+	 * The method is used to get the student 
+	 * 
+	 * @return
+	 */
 	public Student getStudent() {
 		return student;
 	}
 
-
+	/**
+	 * The method is used to set the student 
+	 * 
+	 * @param student
+	 */
 	public void setStudent(Student student) {
 		this.student = student;
 	}
 
-
+	/**
+	 * The method is used to get the examiner
+	 * 
+	 * @return
+	 */
 	public Pruefer getPruefer() {
 		return pruefer;
 	}
 
-
+	/**
+	 * The method is used to set the examiner
+	 * 
+	 * @param pruefer
+	 */
 	public void setPruefer(Pruefer pruefer) {
 		this.pruefer = pruefer;
 	}
